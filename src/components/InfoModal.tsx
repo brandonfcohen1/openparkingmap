d627fd39f32c32854447a1b37b9009cffa14db84
Brandon Cohen <brandon.f.cohen@gmail.com>
2023-03-24 19:31:37 -0400

    zindex mobile

--- src/components/InfoModal.tsx
+++ src/components/InfoModal.tsx
@@ -60,7 +60,7 @@ export default function InfoModal({
           <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
         </Transition.Child>
 
-        <div className="fixed inset-0 z-10 overflow-y-auto">
+        <div className="fixed inset-0 z-100 overflow-y-auto">
           <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
             <Transition.Child
               as={Fragment}
