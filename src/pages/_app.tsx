5d57341e3b1d7066830b6539dd46af985c394997
Brandon Cohen <brandon.f.cohen@gmail.com>
2023-03-24 09:37:16 -0400

    build fixes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,25 +5,33 @@ import type { AppProps } from "next/app";
 import Script from "next/script";
 import Head from "next/head";
 
-export default function App({ Component, pageProps }: AppProps) {
+const GoogleAnalytics = () => {
   return (
     <>
-      <Head>
-        <title>OpenParkingMap</title>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-KXX93ZWG0M"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-KXX93ZWG0M"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){window.dataLayer.push(arguments);}
           gtag('js', new Date());
 
           gtag('config', 'GA_MEASUREMENT_ID');
         `}
-        </Script>
+      </Script>
+    </>
+  );
+};
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>OpenParkingMap</title>
       </Head>
+      <GoogleAnalytics />
       <Component {...pageProps} />
     </>
   );
