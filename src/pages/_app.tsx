28b2b04312b30d3b0d331b3fce7e707fecd83ed4
Brandon Cohen <brandon.f.cohen@gmail.com>
2023-03-24 09:29:56 -0400

    GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import "@/styles/Map.css";
 import "mapbox-gl/dist/mapbox-gl.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -9,6 +10,19 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>OpenParkingMap</title>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-KXX93ZWG0M"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'GA_MEASUREMENT_ID');
+        `}
+        </Script>
       </Head>
       <Component {...pageProps} />
     </>
