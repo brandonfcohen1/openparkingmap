4721125460f3514a0abe06bab4f0acf8d43f83bb
Brandon Cohen <brandon.f.cohen@gmail.com>
2023-03-27 21:04:02 -0400

    change overpass query

--- src/overpass/overpass.ts
+++ src/overpass/overpass.ts
@@ -4,11 +4,21 @@ import { FeatureCollection, Position, Feature } from "geojson";
 export const overpassQuery = async (bounds: LngLatBounds) => {
   const bbox = `${bounds.getSouth()},${bounds.getWest()},${bounds.getNorth()},${bounds.getEast()}`;
 
+  const restrictTags = [
+    "underground",
+    "multi-storey",
+    "sheds",
+    "garage_boxes",
+    "carport",
+  ];
+
   const body = `
   [out:json][bbox:${bbox}];
-(way[amenity=parking][parking=surface];)->.x1;nwr.x1->.result;
-(.result; - .done;)->.result;
-.result out meta geom qt;
+  (way[amenity=parking]${restrictTags
+    .map((tag: string) => `[amenity!~"${tag}"]`)
+    .join("")};)->.x1;nwr.x1->.result;
+  (.result; - .done;)->.result;
+  .result out meta geom qt;
   `;
 
   const response = await fetch("https://overpass-api.de/api/interpreter", {
